4f7045851bebb9ef0c04031edb24ab1f8232eb32
JPThorne <thorne.jean.paul@gmail.com>
2021-09-04 17:05:21 +0200

    Add temperature service test and 1st implementation code

---
+++ tests/TemperatureAlert.Domain.Tests/TemperatureServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace TemperatureAlert.Domain.Tests
+{
+    public class TemperatureServiceTests
+    {
+        public TemperatureServiceTests() { }
+
+        [Fact]
+        public void Test_CanConstruct()
+        {
+            //arrange
+
+            //act
+            var service = new TemperatureService();
+
+            //assert
+            Assert.NotNull(service);
+        }
+    }
+}
