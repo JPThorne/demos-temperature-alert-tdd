6af953d407be6a04e7234371ef216b73d5acde99
JPThorne <thorne.jean.paul@gmail.com>
2021-09-05 16:41:11 +0200

    Write 1st passing test implementation

--- src/TemperatureAlert.Domain/TemperatureService.cs
+++ src/TemperatureAlert.Domain/TemperatureService.cs
@@ -5,13 +5,24 @@ namespace TemperatureAlert.Domain
 {
     public class TemperatureService
     {
+        private ITemperatureRepository Repository { get; init;}
+
         public TemperatureService(ITemperatureRepository repository)
         {
+            Repository = repository;
         }
 
-        public Task<AnalysisResult> AnalyzeTemperature(string deviceId, decimal abnormalTemperature)
+        public async Task<AnalysisResult> AnalyzeTemperature(string deviceId, decimal temperature)
         {
-            throw new NotImplementedException();
+            var temperatureRule = await Repository.GetNormalTemperatureRange(deviceId);
+
+            await Repository.RecordTemperatureAnomaly(deviceId, temperature);
+
+            return new AnalysisResult
+            {
+                Status = "Abnormal",
+                Message = "45,534234 was higher than allowed maximum: 35"
+            };
         }
     }
 }
